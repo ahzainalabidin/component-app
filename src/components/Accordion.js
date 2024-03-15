fd73cac563f6403b5ca7bb194fa1b4c96caa8952
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-15 15:25:30 +0800

    Add styling to accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -9,23 +9,26 @@ function Accordion({ items }) {
 
         const isExpanded = index === expandedIndex;
 
-        const icon = <span>
-            {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
-        </span>
+        const icon = (
+            <span className="text-2xl">
+                {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+            </span>
+        );
 
         return (
             <div key={item.id}>
-                <div onClick={() => { setExpandedIndex(index) }}>
-                    {icon}
+                <div className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+                    onClick={() => { setExpandedIndex(index) }}>
                     {item.label}
+                    {icon}
                 </div>
-                {isExpanded && <div>{item.content}</div>}
+                {isExpanded && <div className="border-b p-5">{item.content}</div>}
             </div>
         );
 
     });
 
-    return <div>{renderedItems}</div>;
+    return <div className="border-x border-t rounded">{renderedItems}</div>;
 
 }
 
