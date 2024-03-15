6d654abcec6b3fe6d7d01396ee726b5c49ea2f66
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-15 14:31:51 +0800

    Add items for accordion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import Accordion from "./components/Accordion";
 import ButtonPage from "./pages/ButtonPage";
 
 function App() {
+
+  const items = [
+    {
+      id: '1',
+      label: 'Can I use React on a project?',
+      content: 'You can use on any project you want.'
+    },
+    {
+      id: '2',
+      label: 'Can I use JavaScript on a project?',
+      content: 'You can use on any project you want.'
+    },
+    {
+      id: '3',
+      label: 'Can I use CSS on a project?',
+      content: 'You can use on any project you want.'
+    },
+  ];
+
   return (
     <div>
-      <ButtonPage />
+      <Accordion items={items} />
     </div>
   );
+
 }
 
 export default App;
