e213a737b51b6fc4ba578ce5c3c31335ba9099fd
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-15 11:03:21 +0800

    Add styling for outline with text colours

--- src/Button.js
+++ src/Button.js
@@ -12,11 +12,19 @@ function Button({
 }) {
 
     const classes = className('px-3 py-1.5 border', {
-        'px-border-blue-500 bg-blue-500 text-white': primary,
-        'px-border-gray-900 bg-gray-900 text-white': secondary,
-        'px-border-green-500 bg-green-500 text-white': success,
-        'px-border-yellow-400 bg-yellow-400 text-white': warning,
-        'px-border-red-600 bg-red-500 text-white': danger,
+        'border-blue-500 bg-blue-500': primary,
+        'border-gray-900 bg-gray-900': secondary,
+        'border-green-500 bg-green-500': success,
+        'border-yellow-400 bg-yellow-400': warning,
+        'border-red-500 bg-red-500': danger,
+        'rounded-full': rounded,
+        'bg-white': outline,
+        'text-white': !outline,
+        'text-blue-500': outline && primary,
+        'text-gray-900': outline && secondary,
+        'text-green-500': outline && success,
+        'text-yellow-400': outline && warning,
+        'text-red-500': outline && danger
     });
 
     return <button className={classes}>{children}</button>;
