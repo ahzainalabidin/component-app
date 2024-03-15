4115223661bae080bbf39842b1faa6a4d2e918c0
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-15 12:26:11 +0800

    Reorganize project folder

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
-import Button from "./Button";
-import { GoBell } from "react-icons/go";
+import ButtonPage from "./pages/ButtonPage";
 
 function App() {
   return (
     <div>
-
-      <div>
-        <Button secondary outline rounded>
-          <GoBell />
-          Click me!!
-        </Button>
-      </div>
-
-      <div>
-        <Button danger outline>
-          Buy Now!
-        </Button>
-      </div>
-
-      <div>
-        <Button warning>
-          See Deal!
-        </Button>
-      </div>
-
-      <div>
-        <Button secondary outline>
-          Hide Ads!
-        </Button>
-      </div>
-
-      <div>
-        <Button primary rounded>
-          Something!
-        </Button>
-      </div>
-
+      <ButtonPage />
     </div>
   );
 }
---
+++ src/pages/ButtonPage.js
@@ -0,0 +1,43 @@
+import { GoBell } from "react-icons/go";
+import Button from "../components/Button";
+
+function ButtonPage() {
+    return (
+        <div>
+
+            <div>
+                <Button secondary outline rounded>
+                    <GoBell />
+                    Click me!!
+                </Button>
+            </div>
+
+            <div>
+                <Button danger outline>
+                    Buy Now!
+                </Button>
+            </div>
+
+            <div>
+                <Button warning>
+                    See Deal!
+                </Button>
+            </div>
+
+            <div>
+                <Button secondary outline>
+                    Hide Ads!
+                </Button>
+            </div>
+
+            <div>
+                <Button primary rounded>
+                    Something!
+                </Button>
+            </div>
+
+        </div>
+    );
+}
+
+export default ButtonPage;
