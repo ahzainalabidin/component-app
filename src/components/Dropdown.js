167de9217d73fd910cece31b213c37bfd661847f
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-18 12:25:29 +0800

    Close dropdown menu when click outside it

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,10 +1,33 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { GoChevronDown } from "react-icons/go";
 import Panel from "./Panel";
 
 function Dropdown({ options, value, onChange }) {
 
     const [isOpen, setIsOpen] = useState(false);
+    const divEl = useRef();
+
+    useEffect(() => {
+
+        const handler = (event) => {
+
+            if (!divEl.current) {
+                return;
+            }
+
+            if (!divEl.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+
+        };
+
+        document.addEventListener('click', handler, true);
+
+        return () => {
+            document.removeEventListener('click', handler);
+        };
+
+    }, []);
 
     const handleClick = () => {
         setIsOpen(!isOpen);
@@ -24,7 +47,7 @@ function Dropdown({ options, value, onChange }) {
     });
 
     return (
-        <div className="w-48 relative">
+        <div ref={divEl} className="w-48 relative">
             <Panel className="flex justify-between items-center cursor-pointer" onClick={handleClick}>
                 {value?.label || "Select..."}
                 <GoChevronDown className="text-lg" />
