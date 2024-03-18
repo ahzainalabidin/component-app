94490c45a26a897e624dbe62367af667bfadd492
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-18 16:07:29 +0800

    Separate styling for background and content of modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,7 +2,8 @@ function Modal() {
 
     return (
         <div>
-            Modal!
+            <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
+            <div className="absolute inset-40 p-10 bg-white">I'm a modal!</div>
         </div>
     );
 
