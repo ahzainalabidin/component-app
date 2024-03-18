392490342cf6ad5685af0facc253abeb09b6ac6a
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-18 09:55:29 +0800

    Refactor null option to be more concise

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -21,16 +21,11 @@ function Dropdown({ options, selection, onSelect }) {
         );
     });
 
-    let content = "Select...";
-
-
-    if (selection) {
-        content = selection.label;
-    }
-
     return (
         <div>
-            <div onClick={handleClick}>{content}</div>
+            <div onClick={handleClick}>
+                {selection?.label || "Select..."}
+            </div>
             {isOpen && <div>{renderedOptions}</div>}
         </div>
     );
