60cb568659d71e990fe71361964681c3ef71728f
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-18 14:27:26 +0800

    Create route component to display children based on path name

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import Link from "./components/Link";
+import Route from "./components/Route";
 import AccordionPage from "./pages/AccordionPage";
 import ButtonPage from "./pages/ButtonPage";
 import DropdownPage from "./pages/DropdownPage";
@@ -10,6 +11,19 @@ function App() {
     <div>
       <Link to="/accordion">Go to accordion</Link>
       <Link to="/dropdown">Go to dropdown</Link>
+
+      <div>
+
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+
+        <Route path="/dropdown">
+          <DropdownPage />
+        </Route>
+
+      </div>
+
     </div>
   );
 
