94ddba45d1f444e9f763acd44d8f225383f2779e
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-18 08:48:18 +0800

    Create AccordionPage

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import Accordion from "./components/Accordion";
+import AccordionPage from "./pages/AccordionPage";
 import ButtonPage from "./pages/ButtonPage";
 
 function App() {
 
-  const items = [
-    {
-      id: '1',
-      label: 'Can I use React on a project?',
-      content: 'You can use on any project you want.'
-    },
-    {
-      id: '2',
-      label: 'Can I use JavaScript on a project?',
-      content: 'You can use on any project you want.'
-    },
-    {
-      id: '3',
-      label: 'Can I use CSS on a project?',
-      content: 'You can use on any project you want.'
-    },
-  ];
-
   return (
     <div>
-      <Accordion items={items} />
+      <AccordionPage />
+      <ButtonPage />
     </div>
   );
 
---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,31 @@
+import Accordion from "../components/Accordion";
+
+function AccordionPage() {
+
+    const items = [
+        {
+            id: '1',
+            label: 'Can I use React on a project?',
+            content: 'You can use on any project you want.'
+        },
+        {
+            id: '2',
+            label: 'Can I use JavaScript on a project?',
+            content: 'You can use on any project you want.'
+        },
+        {
+            id: '3',
+            label: 'Can I use CSS on a project?',
+            content: 'You can use on any project you want.'
+        },
+    ];
+
+    return (
+        <div>
+            <Accordion items={items} />
+        </div>
+    );
+
+}
+
+export default AccordionPage;
