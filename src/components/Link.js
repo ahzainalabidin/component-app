5359a2cda0d36d860d60b616afe82aca4b5559df
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-18 14:33:27 +0800

    Open a new tab when press ctrl + click link

--- src/components/Link.js
+++ src/components/Link.js
@@ -6,11 +6,17 @@ function Link({ to, children }) {
     const { navigate } = useContext(NavigationContext);
 
     const handleClick = (event) => {
+
+        if (event.ctrlKey || event.metaKey) {
+            return;
+        }
+
         event.preventDefault();
         navigate(to);
+
     };
 
-    return <a onClick={handleClick}>{children}</a>
+    return <a href={to} onClick={handleClick}>{children}</a>
 
 }
 
