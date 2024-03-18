b3c4e945d4ffdb66e24b8862886634d235c96355
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-18 10:17:05 +0800

    Add styling to dropdown menu

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -15,18 +15,18 @@ function Dropdown({ options, value, onChange }) {
 
     const renderedOptions = options.map((option) => {
         return (
-            <div onClick={() => handleOptionClick(option)} key={option.value}>
+            <div className="hover:bg-sky-100 rounded cursor-pointter p-1" onClick={() => handleOptionClick(option)} key={option.value}>
                 {option.label}
             </div>
         );
     });
 
     return (
-        <div>
-            <div onClick={handleClick}>
+        <div className="w-48 relative">
+            <div className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full" onClick={handleClick}>
                 {value?.label || "Select..."}
             </div>
-            {isOpen && <div>{renderedOptions}</div>}
+            {isOpen && <div className="absolute top-full border rounded p-3 shadow bg-white w-full">{renderedOptions}</div>}
         </div>
     );
 
