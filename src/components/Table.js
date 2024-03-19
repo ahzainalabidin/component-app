8f6815d878d788424ed316f383f4abc2c04e9247
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-19 10:14:05 +0800

    Add data to table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,35 @@
-function Table() {
+function Table({ data }) {
+
+    const renderedRows = data.map((fruit) => {
+
+        return (
+            <tr key={fruit.name}>
+                <td>{fruit.name}</td>
+                <td>{fruit.color}</td>
+                <td>{fruit.score}</td>
+            </tr>
+        );
+
+    });
+
     return (
-        <div>
-            Table
-        </div>
+        <table>
+
+            <thead>
+                <tr>
+                    <th>Fruit</th>
+                    <th>Color</th>
+                    <th>Score</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {renderedRows}
+            </tbody>
+
+        </table>
     );
+
 }
 
 export default Table;
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,11 +1,20 @@
 import Table from "../components/Table";
 
 function TablePage() {
+
+    const data = [
+        { name: 'Orange', color: 'bg-orange-500', score: 5 },
+        { name: 'Apple', color: 'bg-red-500', score: 3 },
+        { name: 'Banana', color: 'bg-yellow-500', score: 1 },
+        { name: 'Lime', color: 'bg-green-500', score: 4 },
+    ];
+
     return (
         <div>
-            <Table />
+            <Table data={data} />
         </div>
     );
+
 }
 
 export default TablePage;
