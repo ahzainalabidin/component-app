3f5d26bbebe14a7c31eec6da721b18d01b7430f9
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-19 09:37:22 +0800

    Display modal wherever user is scrolling

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -16,9 +16,9 @@ function Modal({ onClose, children, actionBar }) {
     return ReactDOM.createPortal(
         <div>
 
-            <div className="absolute inset-0 bg-gray-300 opacity-80" onClick={onClose}></div>
+            <div className="fixed inset-0 bg-gray-300 opacity-80" onClick={onClose}></div>
 
-            <div className="absolute inset-40 p-10 bg-white">
+            <div className="fixed inset-40 p-10 bg-white">
                 <div className='flex flex-col justify-between h-full'>
 
                     {children}
