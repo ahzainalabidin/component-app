160431fc454be708dbcaa0c2ebecddb00e4ea3b3
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-19 09:02:41 +0800

    Add action button inside modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,14 @@
 import ReactDOM from 'react-dom';
 
-function Modal({ onClose }) {
+function Modal({ onClose, children, actionBar }) {
 
     return ReactDOM.createPortal(
         <div>
             <div className="absolute inset-0 bg-gray-300 opacity-80" onClick={onClose}></div>
-            <div className="absolute inset-40 p-10 bg-white">I'm a modal!</div>
+            <div className="absolute inset-40 p-10 bg-white">
+                {children}
+                {actionBar}
+            </div>
         </div>,
         document.querySelector('.modal-container')
     );
--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -14,10 +14,22 @@ function ModalPage() {
         setShowModal(false);
     };
 
+    const actionBar = (
+        <div>
+            <Button primary onClick={handleClose}>I accept</Button>
+        </div>
+    );
+
+    const modal = (
+        <Modal onClose={handleClose} actionBar={actionBar}>
+            <p>Here is an important agreement for you to accept</p>
+        </Modal>
+    );
+
     return (
         <div className="relative">
             <Button primary onClick={handleClick}>Open Modal</Button>
-            {showModal && <Modal onClose={handleClose} />}
+            {showModal && modal}
         </div>
     );
 
