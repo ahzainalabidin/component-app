6c919d55346b537a5eb59e87d4b75d629b3a324b
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-19 09:29:52 +0800

    Disable scrolling when modal is opened

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 function Modal({ onClose, children, actionBar }) {
+
+    useEffect(() => {
+
+        document.body.classList.add('overflow-hidden');
+
+        return () => {
+            document.body.classList.remove('overflow-hidden');
+        };
+
+    }, []);
 
     return ReactDOM.createPortal(
         <div>
