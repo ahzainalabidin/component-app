e76149382f0d89f0f7c208a8f87e3f01866d8a60
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-19 09:08:17 +0800

    Add styling to modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,11 +4,21 @@ function Modal({ onClose, children, actionBar }) {
 
     return ReactDOM.createPortal(
         <div>
+
             <div className="absolute inset-0 bg-gray-300 opacity-80" onClick={onClose}></div>
+
             <div className="absolute inset-40 p-10 bg-white">
-                {children}
-                {actionBar}
+                <div className='flex flex-col justify-between h-full'>
+
+                    {children}
+
+                    <div className='flex justify-end'>
+                        {actionBar}
+                    </div>
+
+                </div>
             </div>
+
         </div>,
         document.querySelector('.modal-container')
     );
