d6e0051ca54893f83c2fc226d70474a4354ef921
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-20 09:01:40 +0800

    Add input and button to add count

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import Button from "../components/Button";
+import Panel from "../components/Panel";
 
 function CounterPage({ initialCount }) {
 
     const [count, setCount] = useState(initialCount);
+    const [valueToAdd, setValueToAdd] = useState();
 
     const increment = () => {
         setCount(count + 1);
@@ -14,11 +16,22 @@ function CounterPage({ initialCount }) {
     };
 
     return (
-        <div>
-            <h1>Count is {count}</h1>
-            <Button outline onClick={increment}>Increment</Button>
-            <Button outline onClick={decrement}>Decrement</Button>
-        </div>
+        <Panel className="m-3">
+
+            <h1 className="text-lg">Count is {count}</h1>
+
+            <div className="flex flex-row">
+                <Button outline onClick={increment}>Increment</Button>
+                <Button outline onClick={decrement}>Decrement</Button>
+            </div>
+
+            <form>
+                <label>Add a lot!</label>
+                <input type="number" className="p-1 m-3 bg-gray-50 border border-gray-300" />
+                <Button outline>Add it!</Button>
+            </form>
+
+        </Panel>
     );
 
 }
