9293cc1351b5e98dae9b7f7da175d2c81688f830
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-20 09:38:17 +0800

    Replace useState with useReducer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,35 +1,47 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
 
+const reducer = (state, action) => {
+    return {
+        ...state,
+        count: state.count + 1,
+    };
+};
+
 function CounterPage({ initialCount }) {
 
-    const [count, setCount] = useState(initialCount);
-    const [valueToAdd, setValueToAdd] = useState(0);
+    // const [count, setCount] = useState(initialCount);
+    // const [valueToAdd, setValueToAdd] = useState(0);
+
+    const [state, dispatch] = useReducer(reducer, {
+        count: initialCount,
+        valueToAdd: 0
+    });
 
     const increment = () => {
-        setCount(count + 1);
+        dispatch();
     };
 
     const decrement = () => {
-        setCount(count - 1);
+        // setCount(count - 1);
     };
 
     const handleChange = (event) => {
         const value = parseInt(event.target.value) || 0;
-        setValueToAdd(value);
+        // setValueToAdd(value);
     };
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        setCount(count + valueToAdd);
-        setValueToAdd(0);
+        // setCount(count + valueToAdd);
+        // setValueToAdd(0);
     };
 
     return (
         <Panel className="m-3">
 
-            <h1 className="text-lg">Count is {count}</h1>
+            <h1 className="text-lg">Count is {state.count}</h1>
 
             <div className="flex flex-row">
                 <Button outline onClick={increment}>Increment</Button>
@@ -41,7 +53,7 @@ function CounterPage({ initialCount }) {
                 <input
                     type="number"
                     className="p-1 m-3 bg-gray-50 border border-gray-300"
-                    value={valueToAdd || ""}
+                    value={state.valueToAdd || ""}
                     onChange={handleChange}
                 />
                 <Button outline>Add it!</Button>
