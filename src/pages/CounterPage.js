e5c6f7f1216bb14cb5a2f375952af79bab49e890
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-20 08:41:26 +0800

    Add decrement button

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import Button from "../components/Button";
-import useCounter from "../hooks/use-counter";
 
 function CounterPage({ initialCount }) {
 
-    const { count, increment } = useCounter(initialCount);
+    const [count, setCount] = useState(initialCount);
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const decrement = () => {
+        setCount(count - 1);
+    };
 
     return (
         <div>
             <h1>Count is {count}</h1>
             <Button outline onClick={increment}>Increment</Button>
+            <Button outline onClick={decrement}>Decrement</Button>
         </div>
     );
 
